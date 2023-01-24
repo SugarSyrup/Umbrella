adcac8757d879e1ebe63d55c0d17fcb4099e67cd
SugarSyrup <tlfvm04@naver.com>
2023-01-24 15:20:54 +0900

    this is dev branch!!!

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import '@/styles/resets.css'
 import Layout from '../components/Layout'
 import type { AppProps } from 'next/app'
 
+//this is dev branch!
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
