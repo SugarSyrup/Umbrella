aec0b7a2c7793e9b98d86111ff59cf901fe1725f
SugarSyrup <tlfvm04@naver.com>
2023-01-25 16:34:56 +0900

    FEAT : WebSocket 채팅 보내기
    WebSocket 으로 통신, 채팅 주고받기 가능

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,33 +1,70 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, FormEvent } from "react";
+import Head from 'next/head';
 
 export default function Chats() {
     const [_messages,setMessages] = useState<string[]>(["Chatting Service is Now starting!"]);
+    const [sendMessage, setSendMessage] = useState<string>("");
+    const [socket, setSocket] = useState<WebSocket>();
+    // const [tmpSocket, _] = useState<WebSocket>(new WebSocket('wss://umbrellawss.fly.dev/'));
+
+    // useEffect(() => {
+    // const socket = new WebSocket('wss://umbrellawss.fly.dev/');
+    //     socket.addEventListener("open", () => {
+    //         console.log('✔webSocket is Connected!✔');
+    //     });
+    //     socket.addEventListener("close", () => {
+    //         console.log('❌webSocket Connection is Closed!❌');
+    //     });
+    //     socket.addEventListener("message", (message) => {
+    //         setMessages(prev => [...prev, message.data]);
+    //         console.log(_messages);
+    //     });
+    // }, []);
+    
     useEffect(() => {
-        const socket = new WebSocket('wss://umbrellawss.fly.dev/');
-        socket.addEventListener("open", () => {
+        const _socket = new WebSocket('wss://umbrellawss.fly.dev/');
+        setSocket(_socket);
+        _socket.addEventListener("open", () => {
             console.log('✔webSocket is Connected!✔');
         });
-        socket.addEventListener("close", () => {
+        _socket.addEventListener("close", () => {
             console.log('❌webSocket Connection is Closed!❌');
         });
-        socket.addEventListener("message", (message) => {
+        _socket.addEventListener("message", (message) => {
             setMessages(prev => [...prev, message.data]);
+            console.log(_messages);
         });
     }, []);
-    
+
+    const onSubmit = (event:FormEvent) => {
+        event.preventDefault();
+        if(socket !== undefined) {
+            socket.send(JSON.stringify({type:"new_message",payload:sendMessage}));
+        }
+    }
+    const onChange = (event:React.ChangeEvent<HTMLInputElement>) => {
+        event.preventDefault();
+        setSendMessage(event.target.value);
+    }
+
     return (<>
-        <h3>Chatting</h3>
+        <Head>
+            <title>Chat</title>
+            <link rel="stylesheet" href="https://unpkg.com/mvp.css@1.12/mvp.css" /> 
+        </Head>
+        <h1>Chatting</h1>
         {
             _messages.map((message) => {
-                return (<>
-                    <span key={message}>{message}</span>
+                return (<div key={message}>
+                    <span >{message}</span>
                     <br />
-                </>)
+                </div>)
             })
         }
         <br />
-        <form>
-            <input type="text" />
+        <form onSubmit={onSubmit}>
+            <input type="text" placeholder="input your text" style={{width:'50%'}} onChange={onChange}/>
+            <input type="submit" value="전송" />
         </form>
     </>)
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Head from 'next/head'
+import Head from 'next/head';
 import axios, {AxiosError, AxiosResponse} from 'axios';
 
 import {useForm} from 'react-hook-form';
