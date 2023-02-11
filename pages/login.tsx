1e38ce5c72e49adc8816009a2953c1901b660979
SugarSyrup <tlfvm04@naver.com>
2023-02-11 22:17:53 +0900

    FEAT : input 정규식 설정 -> 보안 처리

--- pages/login.tsx
+++ pages/login.tsx
@@ -16,14 +16,14 @@ import { RiKakaoTalkFill } from 'react-icons/ri';
 const schema = Yup.object({
   username: Yup.string().email('email 형식을 입력해주세요').required('이메일(아이디)를 입력해 주세요'),
   password: Yup.string().matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@!%*#?&])[A-Za-z\d@!%*#?&]{8,}$/,'8글자 이상 염문자, 숫자, 특수문자를 조합해서 입력하세요').required('비밀번호를 입력해 주세요'),
-}).required();
+});
 type FormData = Yup.InferType<typeof schema>;
 
 export default function Home() {
   const JWT_EXPIRY_TIME = 1 * 3600 * 1000; // 만료 시간 (24시간 밀리 초로 표현)
 
   const [toggleLogin, setToggleLogin] = useState<boolean>(true);
-  const {register, handleSubmit, formState: {errors}} = useForm({
+  const {register, handleSubmit, formState: {errors}} = useForm<FormData>({
     resolver: yupResolver(schema)
   });
 
@@ -71,7 +71,6 @@ export default function Home() {
             <span className="smallTxt">New to Umbrella? <a>Create an new account</a></span>
         </div>
         <form onSubmit={handleSubmit(onSubmit)}>
-          {/* todo : 정규식으로 이메일 비밀번호 양식 제한하기 */}
           <input {...register('username')} placeholder='Email' />
           {errors.username && <p style={{color:'red'}}>{errors.username?.message}</p>}
           <input {...register('password')} type="password" placeholder='Password' />
