2db357edad7c38a247b15aac9172fe6d2b781583
SugarSyrup <tlfvm04@naver.com>
2023-03-01 12:41:46 +0900

    불필요한 주석 제거

--- tests/user/login.test.tsx
+++ tests/user/login.test.tsx
@@ -33,38 +33,3 @@ describe('Login Page', () => {
         });
     })
 })
-
-//https://dev-yakuza.posstree.com/ko/react/nextjs/test/
-//https://nextjs.org/docs/testing#setting-up-jest-with-the-rust-compiler
-
-
-// error 해결 1
-
-
-// Configuration error:
-    
-// Could not locate module @/pages/user/login mapped as:
-// C:\WorkSpace\umbrella\src\$1.
-
-// Please check your configuration for these entries:
-// {
-//   "moduleNameMapper": {
-//     "/@\/(.*)$/": "C:\WorkSpace\umbrella\src\$1"
-//   },
-//   "resolver": undefined
-// }
-
-// https://github.com/vuejs/vue-jest/issues/40
-
-
-//error 2
-
-//  Test environment jest-enviroment-jsdom cannot be found. Make sure the testEnvironment configuration option points to an existing node module.
-
-// or
-
-// Implicit config resolution does not allow multiple configuration files.
-// Either remove unused config files or select one explicitly with `--config`.        
-
-
-//https://stackoverflow.com/questions/72013449/upgrading-jest-to-v29-error-test-environment-jest-environment-jsdom-cannot-be
