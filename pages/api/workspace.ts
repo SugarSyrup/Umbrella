286200bbbfc7dbbf507b6fe0a212a85ea4650fa3
SugarSyrup <tlfvm04@naver.com>
2023-04-10 13:44:34 +0900

    FEAT : WorkSpace Page, state management

---
+++ pages/api/workspace.ts
@@ -0,0 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { id, UserData, UserDB } from './data';
+
+export default function handler (req: NextApiRequest, res: NextApiResponse)  {
+    if(req.method === 'GET'){
+        return res.status(200).send({
+            data: ['likelion', 'GDSC']
+        });
+    }
+
+    res.status(405).send({message : 'wrong connect'});
+}
