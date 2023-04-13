c5e77e940272b68de5541cc8f7e2b3d94489f43b
SugarSyrup <tlfvm04@naver.com>
2023-04-13 14:51:26 +0900

    FEAT : Some Lotties

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,19 @@ import { wrapper } from 'store';
 import '@/styles/resets.scss'
 import axios from 'axios'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components';
+
+const defaultTheme = {
+
+}
 
 axios.defaults.withCredentials = true;
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
 
