41463550f84e2707edf236eb624485ce682fdc0f
SugarSyrup <tlfvm04@naver.com>
2023-05-30 17:16:35 +0900

    Fix: Login Bug

--- components/molecules/User/InputWithErrorMessage.tsx
+++ components/molecules/User/InputWithErrorMessage.tsx
@@ -20,6 +20,7 @@ export const InputWithErrorMessage = ({inputProps, errorMessage, customref} : In
     return(
         <InputWithErroMessageDiv>
             <label style={{fontSize:'12px', fontWeight:'bold'}}>{inputProps.name}</label>
+            {/* 왜 ref있으면 login 로직이 제대로 동작 안하지? */}
             {/* <AuthFormInput {...inputProps} ref={customref}/> */}
             <AuthFormInput {...inputProps} />
             { errorMessage ? <SmallErrorMessage>{errorMessage}</SmallErrorMessage> : <EmptySpace /> }
