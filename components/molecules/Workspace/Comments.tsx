2c5d34897a9aa02ba96c5b23c28c7a2f80429626
SugarSyrup <tlfvm04@naver.com>
2023-06-14 13:54:35 +0900

    FEAT : BOARD VIEW

---
+++ components/molecules/Workspace/Comments.tsx
@@ -0,0 +1,55 @@
+import useAxios from "@/components/businesses/useAxios";
+import { Form, Button, Input } from "antd";
+import React from "react";
+
+
+interface IComments {
+    id: number;
+}
+
+export function Comments({id} : IComments) {
+    const {response, error, loading} = useAxios({ 
+        method: `GET`,
+        url: `/posts/{id}/comments`,
+        headers : {
+            "Content-Type" : "application/json",
+        }
+    })
+    const [comments, setCommnets] = React.useState<{
+        commentId : number,
+        content : string,
+        nickName : string
+    }[]>([])
+
+    
+    const onFinish = (values: any) => {
+        console.log('Received values from form: ', values);
+    };
+
+    React.useEffect(() => {
+        if(response) {
+            setCommnets(response.data);
+        }
+        
+        else if(error) {
+            console.log(error)
+        }
+    }, [response, error])
+    return <>
+        <Form
+            name="comments_form"
+            layout="inline"
+            onFinish={onFinish}
+        >
+            <Form.Item name="comment" label="Comment" >
+                <Input 
+                    type="text"
+                    style={{ width: 100 }}
+                />
+            </Form.Item> 
+            <Button type="primary" htmlType="submit">
+                Submit
+            </Button>
+        </Form>
+    </>
+}
--- components/organisms/Workspace/BoardView.tsx
+++ components/organisms/Workspace/BoardView.tsx
@@ -2,23 +2,57 @@ import * as React from 'react';
 import styled from 'styled-components';
 import 'react-quill/dist/quill.snow.css';
 import useAxios from '@/components/businesses/useAxios';
+import {Comments} from '@/components/molecules/Workspace/Comments';
+import { useRouter } from 'next/router';
 
-export function BoardView() {
+interface IBoradViewProps {
+    id: number;
+}
+
+export function BoardView({id} : IBoradViewProps) {
     const { response, error, loading, sendData } = useAxios({
         method: `GET`,
-        url: `{POSTID}`,
+        url: `{id}`,
         headers : {
             "Content-Type" : "application/json",
         }
     })
-    const [data, setData] = React.useState(null);
+    const [data, setData] = React.useState<{id:number, title: string, writer: string, content: string, likeCount:number}>();
+    const viewContainerRef = React.useRef<HTMLDivElement>(null);
+    const router = useRouter();
+
+    const onEdit = () => {
+        router.push(`/workspace/board/edit/${id}`)
+    }
+
+    const onDeletePost = () => {
+        if(!confirm("정말 이 게시글을 삭제하시겠습니까?")) {
+            alert("삭제를 취소합니다.")
+        }
+        else {
+            //axios.
+        }
+    }
 
     React.useEffect(() => {
-        setData(response?.data)
+        setData(response?.data);
+        if(viewContainerRef.current){
+            viewContainerRef.current.innerHTML = response?.data?.content;
+        }
     }, [response])
     
     return(<EditorContainer>
-        <h2>data.</h2>
+        <h2>{data?.title}</h2>
+        <span>{data?.writer}</span>
+        <span ref={viewContainerRef}></span>
+        <span>{data?.likeCount}</span>
+        <span onClick={() => {
+            onEdit();
+        }}>수정</span>
+        <span onClick={() => {
+            onDeletePost();
+        }}>삭제</span>
+        <Comments id={data?.id}/>
     </EditorContainer>)
 }
 
